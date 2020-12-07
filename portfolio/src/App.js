f0a4b8df746151ed13ad6382709073bef76c0d56
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-12-07 04:03:30 -0300

    Criação presentation

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Presentation from './components/Presentation';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Presentation />
+    </div>
+  );
+}
+
+export default App;
