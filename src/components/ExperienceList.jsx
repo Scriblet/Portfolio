c746403e09c7a06d441149dccf634f6504757a7e
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-12-07 22:29:11 -0300

    finalização experience]

--- src/components/ExperienceList.jsx
+++ src/components/ExperienceList.jsx
@@ -7,10 +7,16 @@ class ExperienceList extends React.Component {
   render() {
     const { experienceData } = this.props;
     return (
-      <div className="experience-list">
-        {experienceData.map((experience, index) => (
-          <Experience index={index} experienceData={experience} key={experienceData.experience} />
-        ))}
+      <div className="experience-container">
+        <h4 className="experience-tag">Experiência</h4>
+        <h1 className="experience-briefing">
+          Empresas onde trabalhei e Aprendizados que conquistei
+        </h1>
+        <div className="experience-list">
+          {experienceData.map((experience, index) => (
+            <Experience index={index} experienceData={experience} key={experienceData.experience} />
+          ))}
+        </div>
       </div>
     );
   }
